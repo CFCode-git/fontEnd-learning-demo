6699f86f436b9f9581062f3aa1de5731a5af3c9a
CFCode-git <435184766@qq.com>
2021-03-19 00:13:25 +0800

    手写bind： 使用旧语法实现

--- this/bind/src/index.js
+++ this/bind/src/index.js
@@ -1,4 +1,18 @@
-function myBind(asThis, ...args1) {
+var slice = Array.prototype.slice;
+
+function myBind(asThis) {
+  var fn = this;
+  if (typeof fn !== "function") {
+    throw new Error("bind 应该由函数调用");
+  }
+  var args1 = slice.call(arguments, 1);
+  return function () {
+    var args2 = slice.call(arguments, 0);
+    return fn.apply(asThis, args1.concat(args2));
+  };
+}
+
+function _myBind(asThis, ...args1) {
   // 这里的 this 就是 函数
   const fn = this;
   if (typeof fn !== "function") {
