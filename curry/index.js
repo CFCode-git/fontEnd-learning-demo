43a8f817e3f61af6ea1f45b856b56625ef16a66b
CFCode-git <435184766@qq.com>
2021-03-23 09:15:13 +0800

    update：curry

--- curry/index.js
+++ curry/index.js
@@ -1,26 +1,46 @@
-function curry(fn, ...args) {
-  const length = fn.length; // fn 接受的参数个数
-  args = args || [];
-  // console.log(args)
-  return function () {
-    let _args = Array.prototype.slice.call(args, 0);
-    let arg;
-    for (let i = 0; i < arguments.length; i++) {
-      arg = arguments[i];
-      _args.push(arg);
-    }
-    if (_args.length < length) {
-      return curry.call(this, fn, ..._args);
+// function curry(fn, ...args) {
+//   const length = fn.length; // fn 接受的参数个数
+//   args = args || [];
+//   // console.log(args)
+//   return function () {
+//     let _args = Array.prototype.slice.call(args, 0);
+//     let arg;
+//     for (let i = 0; i < arguments.length; i++) {
+//       arg = arguments[i];
+//       _args.push(arg);
+//     }
+//     if (_args.length < length) {
+//       return curry.call(this, fn, ..._args);
+//     } else {
+//       return fn.apply(this, _args);
+//     }
+//   };
+// }
+//
+// function testAdd(a, b, c, d) {
+//   console.log(a + b + c + d);
+//   return a + b + c + d;
+// }
+//
+// const curried = curry(testAdd, 1, 2);
+// curried(4)(5, 6, 7, 8);
+
+function curry(fn, ...args1) {
+  const length = fn.length;
+  args1 = args1 || [];
+  return function (...args2) {
+    let args = args1.concat(args2);
+    if (args.length < length) {
+      return curry.call(this, fn, ...args);
     } else {
-      return fn.apply(this, _args);
+      return fn.call(this, ...args);
     }
   };
 }
-
-function testAdd(a, b, c, d) {
+function add(a, b, c, d) {
   console.log(a + b + c + d);
   return a + b + c + d;
 }
 
-const curried = curry(testAdd, 1, 2);
-curried(4)(5, 6, 7, 8);
+const curried = curry(add)(1)(2);
+curried(3, 4);
