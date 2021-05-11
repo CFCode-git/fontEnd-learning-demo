8f66a66a06f9410a6202e0b4c2c663c53166400a
CFCode-git <435184766@qq.com>
2021-05-11 14:07:41 +0800

    update: curry

--- curry/index.js
+++ curry/index.js
@@ -25,22 +25,48 @@
 // const curried = curry(testAdd, 1, 2);
 // curried(4)(5, 6, 7, 8);
 
-function curry(fn, ...args1) {
-  const length = fn.length;
-  args1 = args1 || [];
-  return function (...args2) {
-    let args = args1.concat(args2);
-    if (args.length < length) {
-      return curry.call(this, fn, ...args);
-    } else {
-      return fn.call(this, ...args);
+{
+  function curry(fn, ...args1) {
+    const length = fn.length
+    args1 = args1 || []
+    return function (...args2) {
+      let args = args1.concat(args2)
+      if (args.length < length) {
+        return curry.call(this, fn, ...args)
+      } else {
+        return fn.call(this, ...args)
+      }
     }
-  };
-}
-function add(a, b, c, d) {
-  console.log(a + b + c + d);
-  return a + b + c + d;
+  }
+
+  function add(a, b, c, d) {
+    console.log(a + b + c + d)
+    return a + b + c + d
+  }
+
+  //
+  // const curried = curry(add)(1)(2)
+  // curried(3, 4)
 }
 
-const curried = curry(add)(1)(2);
-curried(3, 4);
+
+/*箭头函数柯里化*/
+/*
+ const join = (a, b, c) => {
+   return `${a}_${b}_${c}`
+}
+ const curriedJoin = curry2(join)
+ curriedJoin(1, 2, 3) // '1_2_3'
+ curriedJoin(1)(2, 3) // '1_2_3'
+ curriedJoin(1, 2)(3) // '1_2_3'
+*/
+{
+  function curry2(fn) {
+    return function curryInner(...args1) {
+      if (args1.length >= fn.length) return fn(...args1)
+      return (...args2) => curryInner(...args1, ...args2)
+    }
+  }
+}
+
+
